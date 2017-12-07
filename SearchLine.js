42c1756c15e54ea668ccaf656a04309df577d82d
Andrius Urbelis <andrius.urbelis@gmail.com>
2017-12-07 21:21:07 +0200

    npm init

--- SearchLine.js
+++ SearchLine.js
@@ -1,7 +1,6 @@
 
 const fs = require('fs');
 const sr = fs.createReadStream('C:\\temp\\32_20171205001\\32_20171205001.TSYSO');
-
 var perfy = require('perfy');
 
 perfy.start('loop-stuff');
@@ -10,15 +9,9 @@ const lineReader = require('readline').createInterface({
     input: sr
 });
 
-var i = 0;
 
 lineReader.on('line', function(line){
-
-    i++;
-    console.log(i);
-
     if (line[0] === 'T'){
-        console.log(i);
         console.log(line);
     }
 });
